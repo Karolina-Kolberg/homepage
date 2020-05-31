7d45e2532336883d6c336ce8c80a264af2abac80
Karolina Kolberg <k.adamczyk87@gmail.com>
2020-05-31 10:56:58 +0200

    create BEM

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const name= 'Karolina';
-const age= 33;
+const name = "Karolina";
+const age = 33;
 
 console.log(`Hej! Nazywam sie ${name} i mam ${age} lata`);
+
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = 'Karolina Kolberg - subiektywna opinia o kursie WTF';
