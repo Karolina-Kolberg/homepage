c1fc7c32f01dea1ccf2d5f92784ffb50349b14f1
Karolina Kolberg <k.adamczyk87@gmail.com>
2020-06-04 16:10:40 +0200

    created new file html

--- main.js
+++ main.js
@@ -4,6 +4,3 @@ const age = 33;
 console.log(`Hej! Nazywam sie ${name} i mam ${age} lata`);
 
 
-const heading = document.querySelector('.main-heading--js');
-
-heading.innerHTML = 'Karolina Kolberg - subiektywna opinia o kursie WTF';
