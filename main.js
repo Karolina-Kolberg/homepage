2e950f4ef1750d1a72f2cfede083537e08c6fccf
Karolina Kolberg <k.adamczyk87@gmail.com>
2020-10-21 15:38:53 +0200

    added Menu

--- main.js
+++ main.js
@@ -19,4 +19,11 @@ greet (age, name)
 
 if('Java' != 'Javascript') {
     console.log('Java to Javascript');                                                                                                                                                                                                                                                                                                                                                                            
-} 
+}
+
+const menu = document.querySelector('.menu--js');
+
+menu.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
