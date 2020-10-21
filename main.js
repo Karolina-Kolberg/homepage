aeaf65ec5f1e66f21facaa5276c4079b8002d2c4
Karolina Kolberg <k.adamczyk87@gmail.com>
2020-10-21 13:42:29 +0200

    added aside, GRID, Media queries

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ const age = 33;
 console.log(`Hej! Nazywam sie ${name} i mam ${age} lata`);
 
 
+const emptyParagraph = document.querySelector('.notes-creation_description--js');
+
+emptyParagraph.innerHTML = `Nawet uzylam kolorowych zakreslaczy.`;
+
+
+function greet (age,name) {
+    console.log(`Hej! Nazywam sie ${name} i mam ${age} lata`);
+}
+
+greet (age, name)
+
+
+
+if('Java' != 'Javascript') {
+    console.log('Java to Javascript');                                                                                                                                                                                                                                                                                                                                                                            
+} 
